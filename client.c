67a456a25bd0abf12437b4d2b4864c5d48859f6f
Qwmyee <qwmyee@gmail.com>
2020-11-23 16:25:40 +0200

    send/receive chunks of data

--- client.c
+++ client.c
@@ -16,51 +16,70 @@
 
 int main(int argc, char **argv) {
 
-	/*
-	 * check if the number of arguments are correct
-	 */
-	// if (argc != 2) {
-	// 	printf("Incorrect usage!\n");
-	// 	printf("Should be called: ./client <filepath_name>\n");
-	// 	return -1;
-	// }
-
+	/* client and server DS */
+	int client_d;
+	struct sockaddr_in server;
 
 	/*
-	 * get path of file from cli rather than from the argv
+	 * setup server
 	 */
-	size_t size = PATH_MAX;
-	char *filepath_name = (char*)malloc(sizeof(char)*size + 1);
-	/* REVIEW: don't forget to free this */
-	size_t filepath_size;
-	
-	if((filepath_size = getline(&filepath_name, &size, stdin)) == -1) {
-		perror("Unable to read from console.\n");
-		free(filepath_name);
+	memset(&server, 0, sizeof(server));
+	server.sin_port = htons(PORT);
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr(ADDR);
+
+	/*
+	 * NOTE: create client socket & connect to server
+	 */
+	if(
+		((client_d = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	||	((connect(client_d, (struct sockaddr *) &server, sizeof(server))) == -1)
+	) {
+		perror("Error");
 		return -1;
 	}
-	// getline keeps the '\n' if exists, delete it
-	if(filepath_name[filepath_size-1] == '\n') filepath_size--;
-	filepath_name[filepath_size]='\0';
-	printf("%s  %ld\n", filepath_name, filepath_size);
+	/* client is connected to server */
 
 	/*
-	 * getcwd gets the absolute path for the current directory (client directory)
+	 * NOTE: getcwd gets the absolute path for the current directory (client directory)
 	 * returns NULL if the current path is larger that PATH_MAX
 	 */
 	char current_directory_absolute_path[PATH_MAX];
 	if (getcwd(current_directory_absolute_path, PATH_MAX) == NULL) {
 		perror("Couldn't get current directory!");
-		free(filepath_name);
 		return -1;
 	} else {
 		printf("Current working directory: %s\n", current_directory_absolute_path);
 	}
 
 	/*
-	 * realpath gets the absolute path for a file that is relative to the current directory
+	 * NOTE: get path of file from stdin
+	 * getline returns -1 if unable to read from given input
+	 */
+	size_t size = PATH_MAX; /* max size for input */
+	size_t filepath_size; /* this will have the actual size of the given input */
+	char *filepath_name = (char*)malloc(sizeof(char)*PATH_MAX + 1);
+	/* REVIEW: don't forget to free this */
+
+	if((filepath_size = getline(&filepath_name, &size, stdin)) == -1) {
+		perror("Unable to read from console.\n");
+		free(filepath_name);
+		return -1;
+	}
+	/* IDEA: getline keeps the '\n' if exists, remove it */
+	if(filepath_name[filepath_size-1] == '\n') filepath_size--;
+	filepath_name[filepath_size]='\0';
+	printf("%s  %ld\n", filepath_name, filepath_size);
+
+	/*
+	 * NOTE: realpath gets the absolute path for a file
+	 * that is relative to the
+	 * current directory
 	 * returns NULL if file is not found
-	 * allocs memory, don't forget to free it
+	 * realpath(...) allocs memory, don't forget to free it
+	 * (FROM MAN) "The resulting pathname is stored as a
+	 * null-terminated string, up to a maximum of
+     * PATH_MAX bytes, in the buffer pointed to by resolved_path."
 	 */
 	char* desired_file_absolute_path;
 	if((desired_file_absolute_path = realpath(filepath_name, NULL)) == NULL){
@@ -69,11 +88,15 @@ int main(int argc, char **argv) {
 		return -1;
 	} else {
 		printf("Path found: %s\n", desired_file_absolute_path);
-		// free(desired_file_absolute_path); // REVIEW: delete this and free after use
 	}
 
-	/* we don't need filepath_name from now on */
+	/*
+	 * IDEA: don't work with given input from now on, use only the absolute paths
+	 */
 	free(filepath_name);
+	/*
+	 * XXX: using filepath_name from here on will probably work but will give size 0
+	 */
 
 	/*
 	 * check if the desired file is inside the current directory
@@ -86,87 +109,99 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	/* client and server DS */
-	int client_d;
-	struct sockaddr_in server;
-
-	/*
-	 * create client socket
-	 */
-	if ((client_d = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("Unable to create client");
-		free(desired_file_absolute_path);
-		return -1;
-	}
-
-	/*
-	 * set server
-	 */
-	memset(&server, 0, sizeof(server));
-	server.sin_port = htons(PORT);
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = inet_addr(ADDR);
-
-	/*
-	 * connect to server
-	 */
-	if ((connect(client_d, (struct sockaddr *) &server, sizeof(server))) == -1) {
-		perror("Unable to connect to server");
-		free(desired_file_absolute_path);
-		return -1;
-	}
-
 	/*
 	 * open given file
+	 * NOTE: this works only if the client has permission to read the file
 	 */
 	int desired_file_descriptor;
 	if ((desired_file_descriptor = open(desired_file_absolute_path, O_RDONLY)) == -1) {
-		perror("File does not exist or cannot be opened");
+		perror("Error");
 		free(desired_file_absolute_path);
 		return -1;
 	}
 
 	/*
-	 * send file
+	 * send file name to server
+	 * NOTE: (from man) "No indication of failure to deliver is implicit in a send().
+	 * Locally detected errors are indicated by a return value of -1."
+	 * XXX: this fails with SIGPIPE if server is closed before sending this
 	 */
 	size_t path_size = strlen(desired_file_absolute_path) + 1;
-	printf("Sending size: %d\n", path_size);
-	printf("Sending path: %s\n", desired_file_absolute_path);
-	send(client_d, &path_size, sizeof(path_size), 0);
-	send(client_d, desired_file_absolute_path, path_size, 0);
+	if (
+		(send(client_d, &path_size, sizeof(path_size), 0) < 0)
+	||	(send(client_d, desired_file_absolute_path, path_size, 0) < 0)
+	) {
+		perror("Unable to send to server");
+		free(desired_file_absolute_path);
+		return -1;
+	} else {
+		printf("Sending size: %ld\n", path_size);
+		printf("Sending path: %s\n", desired_file_absolute_path);
+	}
 
 	/*
 	 * get size of file
 	 * this will be divided into chunks I think
+	 *
+	 * IDEA: let stack free fstat immediately after getting file size
+	 * (maybe the compiler does this anyway if it sees we don't use it anymore idk)
 	 */
+	int file_size;
+	{
 	struct stat st;
 	fstat(desired_file_descriptor, &st);
-	int file_size = st.st_size;
+	file_size = st.st_size;
+	}
 	printf("Size of given file: %d\n", file_size);
 
-	char buf[file_size + 1];
-	ssize_t bytes_read;
 	/*
-	 * read file
+	 * decide the number of chunks to send to the server based on overall size
+	 * using PIPE_BUF (Maximum number of bytes guaranteed to be written automatically
+	 * to a pipe.) from limits.h
 	 */
-	
-	while ((bytes_read = read(desired_file_descriptor, buf, file_size)) > 0) {
-		buf[bytes_read++] = '\0';
-		printf("Read %d bytes: %s\n", bytes_read, buf);
-		/*
-		 * send number of bytes read from file
-		 */
-		send(client_d, &bytes_read, sizeof(bytes_read), 0);
-		/*
-		 * send text read from file
-		 */
-		send(client_d, buf, bytes_read, 0);
+	int bytes_chunks = file_size/(PIPE_BUF-1) + (file_size%(PIPE_BUF-1) != 0);
+	printf("My PIPE_BUF: %d\n", PIPE_BUF);
+	printf("Will send %d chunks.\n", bytes_chunks);
+
+	/*
+	 * send how many chunks will be sent
+	 */
+	if(send(client_d, &bytes_chunks, sizeof(bytes_chunks), 0) <0) {
+		perror("Unable to send to server");
+		free(desired_file_absolute_path);
+		return -1;
 	}
+
+	/*
+	 * read&send file
+	 */
+	char buf[PIPE_BUF];
+	ssize_t bytes_read;
+	while ((bytes_read = read(desired_file_descriptor, buf, PIPE_BUF-1)) > 0) {
+		buf[++bytes_read] = '\0'; /* send the string null-terminated */
+		/* printf("Read %ld bytes: %s\n", bytes_read, buf); */
+		/*
+		 * send size of text & text read from file
+		 * exists with SIGPIPE if server closes randomly
+		 */
+		if(
+			((send(client_d, &bytes_read, sizeof(bytes_read), 0)) < 0)
+		||	((send(client_d, buf, bytes_read, 0)) < 0)
+		) {
+			perror("Error");
+			free(desired_file_absolute_path);
+			return -1;
+		}
+	}
+
+	/* QUESTION: in which case does this fail to read? */
 	if (bytes_read < 0){
 		perror("Error while reading file content");
 		free(desired_file_absolute_path);
 		return -1;
 	}
+
+	/* free everything left */
 	free(desired_file_absolute_path);
 
 	/*
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <unistd.h>
-
+#include <limits.h>
 #define PORT 	1234
 #define ADDR 	"127.0.0.1"
 
@@ -15,14 +15,6 @@ int main() {
 	int client_d;
 	/* client and server DS */
 	struct sockaddr_in server, client;
-
-	/*
-	 * create socket for server
-	 */
-	if ((server_d = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("Unable to create server");
-		return -1;
-	}
 
 	/*
 	 * set server
@@ -35,60 +27,78 @@ int main() {
 	/*
 	 * set client
 	 */
+	socklen_t client_socket_size = sizeof(struct sockaddr_in);
 	memset(&client, 0, sizeof(client));
 
 	/*
-	 * bind server
+	 * create socket for server & bind
 	 */
-	if (bind(server_d, (struct sockaddr *) &server, sizeof(server)) < 0) {
-		perror("Unable to bind server");
+	if (
+		((server_d = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	||	(bind(server_d, (struct sockaddr *) &server, sizeof(server)) < 0)
+	) {
+		perror("Error");
 		return -1;
 	}
 
-	/* listen for connections */
-	listen(server_d, 1);
+	/*
+	 * listen for connections; listen() returns -1 if something goes wrong; 0 otherwise
+	 */
+	if(listen(server_d, 1) == -1) {
+		perror("Error");
+		return -1;
+	}
+
+	/*
+	 * get current directory
+	 * this is null terminated
+	 */
+	char current_directory_absolute_path[PATH_MAX];
+	if (getcwd(current_directory_absolute_path, PATH_MAX) == NULL) {
+		perror("Couldn't get current directory!");
+		return -1;
+	} else {
+		printf("Current working directory: %s\n", current_directory_absolute_path);
+	}
 
 	for(;;) {
 		/* accept client */
-		client_d = accept(server_d, (struct sockaddr *) &client, sizeof(client));
+		client_d = accept(server_d, (struct sockaddr *) &client, &client_socket_size);
 		printf("Client connected.\n");
 		/* receive file path */
 		size_t path_size;
+		/*
+		 * NOTE: recv only send "-1" as error if it was called in a non-blocking manner
+		 * check for error if this is changed
+		 */
 		recv(client_d, &path_size, sizeof(path_size), MSG_WAITALL);
-		printf("Received size: %d\n", path_size);
+		printf("Received size: %ld\n", path_size);
 		char *buffer = (char *) malloc(path_size * sizeof(char));
 		recv(client_d, buffer, path_size, MSG_WAITALL);
 		printf("Received path: %s\n", buffer);
 
-		/* receive text */
-		//TODO: pay attention on receiving multiple chunks from client
-		ssize_t text_size;
-		recv(client_d, &text_size, sizeof(text_size), MSG_WAITALL);
-		printf("Received size: %d\n", text_size);
-		char *buff_text = (char*) malloc(sizeof(char)*text_size);
-		recv(client_d, buff_text, text_size, MSG_WAITALL);
-		printf("Received text: %s\n", buff_text);
-
-		//implement file opening and write data received from client
-		
-		/*get current directory*/
-		char current_directory_absolute_path[PATH_MAX];
-		if (getcwd(current_directory_absolute_path, PATH_MAX) == NULL) {
-			perror("Couldn't get current directory!");
-			free(filepath_name);
-			return -1;
-		} else {
-			printf("Current working directory: %s\n", current_directory_absolute_path);
+		/* receive chunks of bytes */
+		int bytes_chunks;
+		recv(client_d, &bytes_chunks, sizeof(bytes_chunks), MSG_WAITALL);
+		printf("Received chunks: %d\n", bytes_chunks);
+		while(bytes_chunks--) {
+			ssize_t text_size;
+			recv(client_d, &text_size, sizeof(text_size), MSG_WAITALL);
+			printf("Received size: %ld\n", text_size);
+			char *buff_text = (char*) malloc(sizeof(char)*text_size);
+			recv(client_d, buff_text, text_size, MSG_WAITALL);
+			// printf("Received text: %s\n", buff_text);
+			// TODO: implement file opening and write data received from client
+			free(buff_text);
 		}
-		
-		
 
 		/* free buffer memory */
 		free(buffer);
-		free(buff_text);
 
 		/* close client */
 		close(client_d);
 		printf("Client disconnected\n");
 	}
+
+	return 0;
 }
